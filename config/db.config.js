6e65aa06e0349284bc05fd409358035dcfda7920
Francohedi1999 <ramarotafika1999@gmail.com>
2024-06-24 15:39:33 +0300

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -23,58 +23,69 @@ const sequelize_DB = new Sequelize(
 
 const sync_db = async () => {
     try {
-        await sequelize_DB.sync({ force: false });
+        await sequelize_DB.sync({ force: true });
         
         const role_model = require('../models/role.model');
         const user_model = require('../models/user.model');
-        console.log('==================================================================================================');
+        const status_projet_model = require('../models/status_projet.model') ;
+        const mode_projet_model = require('../models/mode_projet.model') ;
+        const nature_projet_model = require('../models/nature_projet.model') ;
+
         console.log('==================================================================================================');
         console.log('Base de données synchronisée');
         console.log('');
-        console.log('');
 
-        console.log('==================================================================================================');
         console.log('==================================================================================================');
         console.log('Création rôle');
         console.log('');
-        console.log('');
 
         await role_model.findOrCreate({ where: { id: 1 } , defaults: { designation: 'Administrateur' } });
-
         await role_model.findOrCreate({ where: { id: 2 }, defaults: { designation: 'Utilisateur' } });
 
-        const password = await bcrypt.hash( "0000" , 10 ) ;
-        const admin = await user_model.findOrCreate({
-        where: { email: 'administrateur@gmail.com' },
-        defaults: {
-            nom: 'Administrateur',
-            prenom: 'Administrateur',
-            email: 'administrateur@gmail.com',
-            password: password ,
-            sexe: 'H',
-            date_naissance: "12/12/1222",
-            id_role: 1,
-            deleted: false
-        }
-        });
 
-        if (admin) 
-        {
         console.log('==================================================================================================');
+        console.log('Création administrateur');
+        console.log('');
+
+        const password = await bcrypt.hash( "0000" , 10 ) ;
+        await user_model.findOrCreate( {
+        where: { email: 'admin@gmail.com' } , 
+        defaults: { nom: 'Administrateur' ,
+                    prenom: 'Administrateur' ,
+                    email: 'admin@gmail.com' ,
+                    password: password ,
+                    sexe: 'H' ,
+                    date_naissance: "12/12/1222" ,
+                    id_role: 1 ,
+                    deleted: false } } ) ;
+        
         console.log('==================================================================================================');
-        console.log('Administrateur créé');
+        console.log('Création administrateur');
         console.log('');
-        console.log('');
-        } 
-        else 
-        {
+
+        await status_projet_model.findOrCreate({ where: { id: 1 } , defaults: { designation: 'Avenir' } });
+        await status_projet_model.findOrCreate({ where: { id: 2 } , defaults: { designation: 'En cours' } });
+        await status_projet_model.findOrCreate({ where: { id: 3 } , defaults: { designation: 'Réalisé' } });
+
         console.log('==================================================================================================');
+        console.log('Création mode de passation');
+        console.log('');
+
+        await mode_projet_model.findOrCreate({  where: { id: 1 } , 
+                                                defaults: { designation: "Appel d'offre ouvert internationnal" , deleted: false } });
+        await mode_projet_model.findOrCreate({  where: { id: 2 } , 
+                                                    defaults: { designation: "Appel à manifestation d'intérêt" , deleted: false } });
+        await mode_projet_model.findOrCreate({  where: { id: 3 } , 
+                                                    defaults: { designation: "Consultation de prix ouverte" , deleted: false } });
+
         console.log('==================================================================================================');
-        console.log('Administrateur existe déjà');
+        console.log('Création nature de passation');
         console.log('');
-        console.log('');
-        }
-  
+
+        await nature_projet_model.findOrCreate({  where: { id: 1 } , 
+                                                defaults: { designation: "Travaux" , deleted: false } });
+        await nature_projet_model.findOrCreate({  where: { id: 2 } , 
+                                                    defaults: { designation: "Prestation intellectuelle" , deleted: false } });
     } 
     catch (error) 
     {
