6903e147803421f4a825d90ee937de4f8740e4e4
Francohedi1999 <ramarotafika1999@gmail.com>
2024-08-19 23:18:49 +0300

    ajout pagination + filtre sur la gestion des passations

--- controllers/passation.controller.js
+++ controllers/passation.controller.js
@@ -1,4 +1,6 @@
 const { passation_model } = require("../migrations") ;
+const { Op } = require("sequelize") ;
+const moment = require("moment") ;
 
 create = async ( req , res ) => 
 {
@@ -32,9 +34,53 @@ get_all_passation = async ( req , res ) =>
 {
     try
     {
-        const passations = await passation_model.findAll();
+        const aut_cont = req.query.aut_cont || "" ;
+        const nom_prmp = req.query.nom_prmp || "" ;
+        const adresse = req.query.adresse || "" ;
+        const date_etab_doc_init = req.query.date_etab_doc_init || "" ;
+        const annee = req.query.annee || "" ;
 
-        return res.status(200).json( passations ) ;
+        const page = req.query.page || 1 ;
+        const limit= req.query.limit || 10;
+
+        const page_number = parseInt( page );
+        const limit_number = parseInt( limit );
+        const offset = ( page_number - 1 ) * limit_number;
+
+        const where_condition = {} ;
+        if (aut_cont) 
+        {
+            where_condition.aut_cont = { [Op.like]: `%${aut_cont}%` } ; 
+        }
+        if (nom_prmp) 
+        {
+            where_condition.nom_prmp = { [Op.like]: `%${nom_prmp}%` } ; 
+        }
+        if (adresse) 
+        {
+            where_condition.adresse = { [Op.like]: `%${adresse}%` } ; 
+        }
+        if (date_etab_doc_init) 
+        {
+            where_condition.date_etab_doc_init =  date_etab_doc_init ;
+        }
+        if (annee) 
+        {
+            where_condition.annee =  annee ;
+        }
+
+        const passations = await passation_model.findAndCountAll({
+            where: where_condition ,
+            limit: limit_number ,
+            offset: offset
+        });
+
+        return res.status(200).json({
+            total_items: passations.count ,
+            data: passations.rows ,
+            current_page: page_number ,
+            total_pages: Math.ceil( passations.count / limit_number )
+        });
     }
     catch( error )
     {
