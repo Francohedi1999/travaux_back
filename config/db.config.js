c1708210cfe4266068d38a5832e683587d1a05a5
Francohedi1999 <ramarotafika1999@gmail.com>
2024-09-13 01:34:02 +0300

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -11,13 +11,13 @@ const sequelize_DB = new Sequelize(
         host: process.env.DB_HOST ,
         dialect: "postgres" ,
         operatorsAliases: 0 ,
-        // pool: 
-        // {
-        //     max: 5 ,
-        //     min: 0 ,
-        //     acquire: 30000 ,
-        //     idle: 10000
-        // }
+        pool: 
+        {
+            max: 5 ,
+            min: 0 ,
+            acquire: 30000 ,
+            idle: 10000
+        }
     }
 ) ;
 
